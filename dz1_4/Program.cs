0220c41c8feefd069f17bc8fde12678c70ea5af3
Erka <ergena89@gmail.com>
2023-04-27 00:34:37 +0800

    ДЗ1,задача 4, решение1

---
+++ dz1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число n =");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 2;
+while (result <= n)
+{
+     Console.WriteLine($"четные числа:{result}");
+     result+=2;
+}
