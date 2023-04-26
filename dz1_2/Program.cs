9ad1510b3617c37b71884e1e25eaf530bf5ad74d
Erka <ergena89@gmail.com>
2023-04-26 13:25:26 +0800

    Дз1, задача 4, первое решение

---
+++ dz1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число a =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b =");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число c =");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b & a > c)
+{
+    Console.Write($"max = {a}");
+    Console.WriteLine(a);
+}
+if(b > a & b > c)
+{
+    Console.WriteLine($"max = {b}");
+   }
+if(c > a & c > b)
+{
+    Console.WriteLine($"max = {c}");
+}
