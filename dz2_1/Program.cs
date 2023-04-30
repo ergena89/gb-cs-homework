1569239416cae8acb50dff71d32fcbf86e99919d
Erka <ergena89@gmail.com>
2023-04-30 12:06:00 +0800

    ДЗ2, решение 10 задачи

---
+++ dz2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: "); 
+int num = Convert.ToInt32(Console.ReadLine());
+int rez = Math.DivRem(num%100,10,out int result);
+Console.WriteLine(rez);
