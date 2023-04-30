721289865cdc562c7aff677db2e3faff8359092a
Erka <ergena89@gmail.com>
2023-04-30 17:43:46 +0800

    ДЗ2, решение задачи 15

---
+++ dz2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите день недели от 1 до 7: "); 
+int den = Convert.ToInt32(Console.ReadLine());
+if (den > 5)
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Нет!");
+}
