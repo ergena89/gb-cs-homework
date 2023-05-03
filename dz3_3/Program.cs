4d787d18552fb953e52aa3b1ce7a52808dfcba9c
Erka <ergena89@gmail.com>
2023-05-04 02:00:17 +0800

    ДЗ 3, решение задачи 23

---
+++ dz3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+double rez = 0;
+while (index <= num)
+{
+    rez = Math.Pow(index, 3);
+    Console.WriteLine($"{rez}");
+    index++;
+}
