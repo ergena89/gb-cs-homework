70688ef14b0f44b8a33a504e407dfc0a19f7f185
Erka <ergena89@gmail.com>
2023-05-04 01:41:29 +0800

    ДЗ 3, решение задачи 21

---
+++ dz3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите координаты точки A: x =");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки A: y =");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки A: z =");
+double az = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты точки B: x =");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки B: y =");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки B: z =");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+
+double result = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2)+ Math.Pow(az - bz, 2));
+Console.Write($"Расстояние между точками = {result}");
