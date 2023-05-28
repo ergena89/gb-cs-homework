ede70a0f806bbaa43025decb851c14283678d220
Erka <ergena89@gmail.com>
2023-05-28 12:44:27 +0800

    Dz5_2,решение задачи 36

---
+++ dz5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// методы
+int[] CreateRandomIntArray(int size, int from, int to)
+        {
+            Random rand = new Random();
+            int[] resultArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                resultArray[i] = rand.Next(from, to);
+            }
+            return resultArray;
+        }
+
+void PrintArray(int[] rand)
+{
+    for (int i = 0; i < rand.Length; i++)
+    {
+        Console.Write($"{rand[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// Решение задачи
+int[] rand = CreateRandomIntArray(7,0,20);
+PrintArray(rand);
+int res = 0;
+for (int i = 0; i < rand.Length; i++)
+{
+    if (i % 2 != 0)
+    res = res + rand[i];
+}
+Console.WriteLine($"сумма элементов, стоящих на нечётных позициях = {res}");
